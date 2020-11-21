d04291b16825bb49f2f7d8be1752fd9875e4844d
miyaParker <karenziboh@gmail.com>
2020-11-21 05:53:02 -0800

    landing page

--- src/components/TopNavBar/TopNavBar.js
+++ src/components/TopNavBar/TopNavBar.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './TopNavBar.css'
 const TopNavBar = () => {
+    const [active,setActive] =useState(3)
+    const handleClick=(event)=>setActive(+event.target.id)
     return (
         <ul className="top-nav-ul">
-            <li className="top-nav-li">About</li>
-            <li className="top-nav-li">Contact</li>
-            <li className="top-nav-li top-nav-active">Tickets</li>
+            <li className={`top-nav-li ${ active===1 ? "top-nav-active":"inactive"}`} id="1" onClick={handleClick}>About</li>
+            <li className={`top-nav-li ${ active===2 ? "top-nav-active":"inactive"}`}  id="2" onClick={handleClick}>Contact</li>
+            <li className={`top-nav-li ${ active===3 ? "top-nav-active":"inactive"}`}  id="3" onClick={handleClick}>Tickets</li>
         </ul>
     )
 }
